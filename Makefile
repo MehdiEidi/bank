add038a395a351a2c05748a6c456ce78024652d2
Mehdi Eidi <mehdiadq@gmail.com>
2022-06-10 12:13:26 +0430

    test: add mock db test for get account api

--- Makefile
+++ Makefile
@@ -27,4 +27,7 @@ tidy:
 server: 
 	go run main.go
 
-.PHONY: postgres createdb dropdb migrateup migratedown sqlc test tidy server
+mock:
+	mockgen -package mockdb -destination db/mock/store.go github.com/mehdieidi/bank/db/sqlc Store
+
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc test tidy server mock
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -20,19 +20,27 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"to_entry"`
 }
 
-type Store struct {
-	*Queries
-	db *sql.DB
+// Store defines all functions to execute db queries and transactions
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
+	db *sql.DB
+	*Queries
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
@@ -75,7 +83,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 	return result, err
 }
 
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
